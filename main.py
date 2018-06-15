bd11fac3135e51be68395ce5330d7da09f01174b
custhila <40281641+custhila@users.noreply.github.com>
2018-06-14 22:00:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ app = Flask(__name__)
 api = Api(app)
 
 class Billpayaccounts(Resource):
-    def get(self, jwttoken, accesstoken):
+    def get(self, account):
         
         try:
             #Get JWT token
-            #url = 'https://nginx0.pncapix.com/Security/v2.0.0/login'
-            #accesstoken = '148a1147-af1d-3cc3-9c52-ff2d309a5a46'
-            #head = {"Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer " + accesstoken}
-            #data = {"password": "swinds258", "username": "swinds258"}
-            #response = json.loads(requests.post(url,json=data,headers=head).text)
-            #jwttoken = response['token']
-            #print("JWTToken retrieved")
+            url = 'https://nginx0.pncapix.com/Security/v2.0.0/login'
+            accesstoken = '148a1147-af1d-3cc3-9c52-ff2d309a5a46'
+            head = {"Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer " + accesstoken}
+            data = {"password": "swinds258", "username": "swinds258"}
+            response = json.loads(requests.post(url,json=data,headers=head).text)
+            jwttoken = response['token']
+            print("JWTToken retrieved")
             
             #check Account
-            #print("Get AccountId")
-            #url = 'https://nginx0.pncapix.com/Account/v2.0.0/account/findByAccountNumber/'+account
-            #head = {"Content-Type": "application/json","Accept": "application/json","X-Authorization": jwttoken, "Authorization": "Bearer " + accesstoken}
-            #response = json.loads(requests.get(url,headers=head).text)
-            #accountId = str(response['accountId'])
-            #print("Retrieved Accoutn ID")
+            print("Get AccountId")
+            url = 'https://nginx0.pncapix.com/Account/v2.0.0/account/findByAccountNumber/'+account
+            head = {"Content-Type": "application/json","Accept": "application/json","X-Authorization": jwttoken, "Authorization": "Bearer " + accesstoken}
+            response = json.loads(requests.get(url,headers=head).text)
+            accountId = str(response['accountId'])
+            print("Retrieved Accoutn ID")
             
             #Get transactions
             print("Get Transactions")
-            url = 'https://nginx0.pncapix.com/Transactions/v2.0.0/transaction?page=0&size=10'
+            url = 'https://nginx0.pncapix.com/Transactions/v2.0.0/transaction/findByAccountId/'+accountId+'?page=0&size=10'
             head = {"Content-Type": "application/json","Accept": "application/json","X-Authorization": jwttoken, "Authorization": "Bearer " + accesstoken}
             response = json.loads(requests.get(url,json=data,headers=head).text)
             print("Transactions retrieved")
@@ -50,7 +50,7 @@ class Billpayaccounts(Resource):
         except:
             return("{\"result\": [{\"ScheduledDate\": \"2018-06-15 21:27:43\", \"Balance\": 1000.0, \"accountId\": 759}, {\"ScheduledDate\": \"2018-06-15 21:27:43\", \"Balance\": 1000.0, \"accountId\": 196}]}")
 
-api.add_resource(Billpayaccounts, "/billpayaccounts/<string:jwttoken>/<string:accesstoken>")
+api.add_resource(Billpayaccounts, "/billpayaccounts/<string:account>")
 
 if __name__ == '__main__':
     app.run(debug=True)
